d9d115f0b0dc03b41bfc798b9d3822a62b1e6784
youssef samir said <76221689+youssefsamir24@users.noreply.github.com>
2023-08-24 17:58:00 +0300

    editied

--- js/main.js
+++ js/main.js
@@ -4,79 +4,111 @@ var passInput = document.getElementById("uPass");
 var emailLog = document.getElementById("email");
 var passLog = document.getElementById("pass");
 var alert = document.getElementById("alert");
-var users = [];
+var alert1 = document.getElementById("alert1");
+let signInAlert = document.getElementById('alert-siginin');
+let users=[];
+let WelcomeUserName = []
+$('#alert1').hide(0);
+$('#alert').hide(0);
+$(signInAlert).hide(0);
+
 //RETRIVE DATA FROM LOCAL STORAGE
 if(localStorage.getItem("data")){
     users = JSON.parse(localStorage.getItem("data"));
 }else{
-    users = [];
+    users=[];
 }
-
 // SIGN UP FUNCTION
 function signUp(){
-    // console.log("mmm");
-    if(localStorage.getItem("data")){
-        for(i=0;i<users.length;i++){
-            if(users[i].email.toLowerCase() === emailInput.value.toLowerCase()){
-                console.log("email");
-                restInputs();
-            }else if(nameInput.value =='' || emailInput.value =='' || passInput.value ==''){
-                console.log("inputs empty");
-                restInputs();
-            }else{
-                var user = {
-                name: nameInput.value,
-                email : emailInput.value,
-                pass : passInput.value
-                }
-                users.push(user);
-                localStorage.setItem("data",JSON.stringify(users));
-                restInputs();
-            }
+    if(users.length!=0){
+        let pass  =  passInput.value;
+        let email =  emailInput.value;
+        let name  =  nameInput.value;
+        if(existEmail(email)!=false && emptyValues(name,email,pass)!=false){
+            $('#alert').hide(500);
+            let userValues = {
+            pass : passInput.value,
+            email : emailInput.value,
+            name  : nameInput.value
         }
-    }else{ 
-        var user = {
-        name: nameInput.value,
-        email : emailInput.value,
-        pass : passInput.value
-        }
-        users.push(user);
+        users.push(userValues)
         localStorage.setItem("data",JSON.stringify(users));
+        $('#alert1').show(1000).hide(2000);
         restInputs();
-
+        }else{
+            $('#alert').show(1000);
+            document.getElementById('email-exist').innerHTML = "This email is already exist, try to Sign in"
+        }
+    }else{
+        let userValues = {
+            pass : passInput.value,
+            email : emailInput.value,
+            name  : nameInput.value
+        }
+        users.push(userValues)
+        localStorage.setItem("data",JSON.stringify(users));
+        $('#alert1').show(1000).hide(2000);
     }
-   
 }
 //END OF SIGN UP FUNCTION
-
 
 //SIGN IN FUNCTION
 
 function signIn(){
-    
-    var signInUsers =JSON.parse(localStorage.getItem("data"));
-    console.log(signInUsers);
 
-    for(i=0;i<signInUsers.length;i++){
-        if(signInUsers[i].email.toLowerCase() == emailLog.value.toLowerCase() && signInUsers[i].pass == passLog.value){
-            console.log(signInUsers[i].pass == passLog.value);
-            console.log(signInUsers[i].email.toLowerCase() == emailLog.value.toLowerCase());
+    for(i=0;i<users.length;i++){
+        if(users[i].email.toLowerCase() == emailLog.value.toLowerCase() && users[i].pass == passLog.value){
+            restInputsLog();
+            $(signInAlert).hide(100);
+            // console.log(users[i].pass == passLog.value);
+            // console.log(users[i].email.toLowerCase() == emailLog.value.toLowerCase());
             console.log("success");
-            restInputsLog();
-        }else{
-            console.log(signInUsers[i].pass == passLog.value);
-            console.log(signInUsers[i].email.toLowerCase() == emailLog.value.toLowerCase());
-            console.log(signInUsers[i].email.toLowerCase());
-            console.log("Fail");
-            restInputsLog();
+            WelcomeUserName.push(users[i].name);
+            console.log(WelcomeUserName);
+            successfullLogin();
+
+            // console.log(typeof gu);
+            // window.open("../pages/welcome.html","_self");
+            // console.log("hello");   
+            // let gu  = WelcomeUserName.pop();
+            // console.log(document.getElementById('user-name'));
+            // document.getElementById('user-name').innerHTML = gu
+            
+            
             
         }
+        // else{
+        //     // console.log(users[i].pass == passLog.value);
+        //     // console.log(users[i].email.toLowerCase() == emailLog.value.toLowerCase());
+        //     // console.log(users[i].email.toLowerCase());
+        //     console.log("Fail");
+        //     $(signInAlert).show(500);
+        //     // restInputsLog();
+            
+        // }
     }
 }
-
-
-
-
+// function successfullLogin() {
+//     window.open("../pages/welcome.html","_self");
+//     console.log("mashy");
+    
+// }
+function existEmail(email) {
+    for (let i = 0; i < users.length; i++) {
+        if(email.toLowerCase()==users[i].email.toLowerCase()){
+            return false;
+        }
+    }
+    return true;
+}
+//function for empty inputs
+function emptyValues(name,email,pass) {
+    if(name =='' && email =='' && pass == ''){
+        return false
+    }
+    return true
+    
+}
 function restInputs(){
     nameInput.value="";
     emailInput.value="";
